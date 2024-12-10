537e1f4b0571fc6a18bac3c5de080e08883703f2
VituuDev <vg221216@gmail.com>
2024-12-10 08:31:13 -0300

    10/12-08:30

--- inicial/inicial.js
+++ inicial/inicial.js
@@ -1,24 +1,35 @@
-function validateTextarea(){
+function validateTextarea(event){
     
     const textarea = document.getElementById('description');
     const errorMessage = document.getElementById('error-message');
     const value = textarea.value.trim();
+    
+
+//impede o envio do formulario se a validação falhar
+event.preventDefault();
+
    //não tem nada no campo
     if(!value){
         errorMessage.textContent = "Este campo é obrigatório.";
         return false;
     }
     //texto menor que 10 caracteres
-    if(value.legth < 10){
+    if(value.length < 10){
         errorMessage.textContent = "A descrição deve ter no mínimo 10 caracteres."
         return false;
     }
     //texto maior que 50 carcteres
-    if(value.legth > 50){
+    if(value.length > 50){
         errorMessage.textContent = "A descrição não pode exceder 50 caracteres."
         return false;
     }
 
     errorMessage.textContent = ""; //Limpa a mensagem de erro
+    alert("Formulário enviado com sucesso!");
     return true;
 }
+ 
+    // Associa a função ao evento submit do formulário
+
+    const form = document.getElementById('form');
+    form.addEventListener('submit', validateTextarea);
--- login/login.js
+++ login/login.js
@@ -1,6 +1,5 @@
-const nome = document.getElementById("name");
-const submitButton = document.getElementById("submitButton");
-
-nome.addEventListener("input", () => {
-  submitButton.disabled = nome.value.trim() === "";
-});
+function validadelogin(){
+  const submitButton = document.getElementById("submitButton");
+  const errorMessage = document.getElementById("error-message");
+  const 
+}
